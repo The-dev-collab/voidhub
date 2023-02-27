50cdd55f92ed9f4c73cf0ac6221354ad411934c3
DanielDuesenDieb <110566119+DanielDuesenDieb@users.noreply.github.com>
2023-02-27 19:21:15 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,8 +3,7 @@ local player = game.Players.LocalPlayer
 repeat wait() until game:IsLoaded() 
 
 if game.PlaceId == 8540346411 then
-  loadstring(game:HttpGet('https://raw.githubusercontent.com/DanielDuesenDieb/voidhub/main/%5BJUNGLE!%5D%20Rebirth%20Champions%20X'))()
-
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/DanielDuesenDieb/voidhub/main/%5BJUNGLE!%5D%20Rebirth%20Champions%20X.lua'))()
 else
   player:kick("WRONG GAME!")
 end
